877bd2e465095e1d17fac924629016dcebec05be
Kayhan <Kayhan@ulu.de>
2024-09-20 09:04:47 +0200

    use Fragments

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -49,7 +49,7 @@ function App() {
           <TabContent topic={selectedTopic}></TabContent>
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
