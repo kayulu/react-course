0ec53221738e85c9922149ca20c9288b1d656e48
Kayhan <Kayhan@ulu.de>
2024-09-24 13:21:50 +0200

    Demo how to avoid unnecessary state management
    use state so that it can be used by multiple components, e.g. a turns array can be used by the Gameboard component to display the gameboard and als by the Log component to display a log of all played turns

--- code/04 React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
   const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X')
+        currentPlayer = 'O';
+
+      const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer},...prevTurns]
+
+      return updatedTurns;
+    })
   }
 
   return (
@@ -16,9 +27,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ul>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
-      LOG
+      <Log turns={gameTurns} />
     </main>
   );  
 }
--- code/04 React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,71 +6,64 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
   const [winner, setWinner] = useState(null);
 
-  function handleButtonClick(rowIndex, colIndex) {
+  let gameBoard = initialGameBoard;
 
-    // uses functional state update pattern
-    setGameBoard((prevBoard) => {
-      const newBoard = [...prevBoard];  // shallow copy of prevBoard, each row hold ref to inner arrays
-      newBoard[rowIndex] = [...newBoard[rowIndex]]; // deep copy only of newBoard[rowIndex]
-      newBoard[rowIndex][colIndex] = activePlayerSymbol;
+  // derive gameBoard from state (turns) that is managed in App component
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
-      // this is another solution, it creates a deep copy of all the elements 
-      // const newBoard = [...prevBoard.map(innerArray => [...innerArray])];
-      // newBoard[rowIndex][colIndex] = activePlayerSymbol;
-
-      checkWin(newBoard, rowIndex, colIndex); // check win works on the newly created array
-
-      return newBoard;
-    })
-
-    onSelectSquare();
+    gameBoard[row][col] = player;
   }
 
-  function checkWin(board, rowIndex, colIndex) {
-    if (checkRowWin(board, rowIndex) || checkColWin(board, colIndex) || checkDiagonalWin(board)) {
-      setWinner(activePlayerSymbol);
-      console.log("Winner is: " + activePlayerSymbol);
-    }
-  }
+  // function checkWin(board, rowIndex, colIndex) {
+  //   if (
+  //     checkRowWin(board, rowIndex) ||
+  //     checkColWin(board, colIndex) ||
+  //     checkDiagonalWin(board)
+  //   ) {
+  //     setWinner(activePlayerSymbol);
+  //     console.log("Winner is: " + activePlayerSymbol);
+  //   }
+  // }
 
-  function checkRowWin(board, rowIndex) {
-    let count = 0;
-    for (const elem of board[rowIndex])
-      elem === activePlayerSymbol ? count++ : null;
+  // function checkRowWin(board, rowIndex) {
+  //   let count = 0;
+  //   for (const elem of board[rowIndex])
+  //     elem === activePlayerSymbol ? count++ : null;
 
-    if (count === 3) return true;
+  //   if (count === 3) return true;
 
-    return false;
-  }
+  //   return false;
+  // }
 
-  function checkColWin(board, colIndex) {
-    let count = 0;
-    board.forEach((row) => {
-      if (row[colIndex] == activePlayerSymbol) count++;
-    });
+  // function checkColWin(board, colIndex) {
+  //   let count = 0;
+  //   board.forEach((row) => {
+  //     if (row[colIndex] == activePlayerSymbol) count++;
+  //   });
 
-    if (count === 3) return true;
+  //   if (count === 3) return true;
 
-    return false;
-  }
+  //   return false;
+  // }
 
-  function checkDiagonalWin(board) {
-    console.log(board);
-    if (
-      (board[0][0] === board[1][1] &&
-        board[1][1] === board[2][2] &&
-        board[0][0] !== null) ||
-      (board[0][2] === board[1][1] &&
-        board[1][1] === board[2][0] &&
-        board[0][2] !== null)
-    )
-      return true;
-    return false;
-  }
+  // function checkDiagonalWin(board) {
+  //   console.log(board);
+  //   if (
+  //     (board[0][0] === board[1][1] &&
+  //       board[1][1] === board[2][2] &&
+  //       board[0][0] !== null) ||
+  //     (board[0][2] === board[1][1] &&
+  //       board[1][1] === board[2][0] &&
+  //       board[0][2] !== null)
+  //   )
+  //     return true;
+  //   return false;
+  // }
 
   return (
     <ol id="game-board">
@@ -79,7 +72,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
           <ol>
             {row.map((activePlayerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleButtonClick(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {activePlayerSymbol}
                 </button>
               </li>
