c568a0d1392639a687c4c13d4fc51f4d781ced0c
Kayhan <Kayhan@ulu.de>
2024-09-25 12:02:46 +0200

    Disable buttons conditionally

--- code/04 React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -11,7 +11,53 @@ function deriveActivePlayer(prevTurns) {
   return currentPlayer;
 }
 
-function App() {
+  // function checkWin(board, rowIndex, colIndex) {
+  //   if (
+  //     checkRowWin(board, rowIndex) ||
+  //     checkColWin(board, colIndex) ||
+  //     checkDiagonalWin(board)
+  //   ) {
+  //     setWinner(activePlayerSymbol);
+  //     console.log("Winner is: " + activePlayerSymbol);
+  //   }
+  // }
+
+  // function checkRowWin(board, rowIndex) {
+  //   let count = 0;
+  //   for (const elem of board[rowIndex])
+  //     elem === activePlayerSymbol ? count++ : null;
+
+  //   if (count === 3) return true;
+
+  //   return false;
+  // }
+
+  // function checkColWin(board, colIndex) {
+  //   let count = 0;
+  //   board.forEach((row) => {
+  //     if (row[colIndex] == activePlayerSymbol) count++;
+  //   });
+
+  //   if (count === 3) return true;
+
+  //   return false;
+  // }
+
+  // function checkDiagonalWin(board) {
+  //   console.log(board);
+  //   if (
+  //     (board[0][0] === board[1][1] &&
+  //       board[1][1] === board[2][2] &&
+  //       board[0][0] !== null) ||
+  //     (board[0][2] === board[1][1] &&
+  //       board[1][1] === board[2][0] &&
+  //       board[0][2] !== null)
+  //   )
+  //     return true;
+  //   return false;
+  // }
+
+export default function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
@@ -39,5 +85,3 @@ function App() {
     </main>
   );  
 }
-
-export default App;
--- code/04 React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 React Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -7,9 +7,7 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard({ onSelectSquare, turns }) {
-  const [winner, setWinner] = useState(null);
-
-  let gameBoard = initialGameBoard;
+  let gameBoard = initialGameBoard.map(row => [...row]);
 
   // derive gameBoard from state (turns) that is managed in App component
   for (const turn of turns) {
@@ -19,61 +17,15 @@ export default function GameBoard({ onSelectSquare, turns }) {
     gameBoard[row][col] = player;
   }
 
-  // function checkWin(board, rowIndex, colIndex) {
-  //   if (
-  //     checkRowWin(board, rowIndex) ||
-  //     checkColWin(board, colIndex) ||
-  //     checkDiagonalWin(board)
-  //   ) {
-  //     setWinner(activePlayerSymbol);
-  //     console.log("Winner is: " + activePlayerSymbol);
-  //   }
-  // }
-
-  // function checkRowWin(board, rowIndex) {
-  //   let count = 0;
-  //   for (const elem of board[rowIndex])
-  //     elem === activePlayerSymbol ? count++ : null;
-
-  //   if (count === 3) return true;
-
-  //   return false;
-  // }
-
-  // function checkColWin(board, colIndex) {
-  //   let count = 0;
-  //   board.forEach((row) => {
-  //     if (row[colIndex] == activePlayerSymbol) count++;
-  //   });
-
-  //   if (count === 3) return true;
-
-  //   return false;
-  // }
-
-  // function checkDiagonalWin(board) {
-  //   console.log(board);
-  //   if (
-  //     (board[0][0] === board[1][1] &&
-  //       board[1][1] === board[2][2] &&
-  //       board[0][0] !== null) ||
-  //     (board[0][2] === board[1][1] &&
-  //       board[1][1] === board[2][0] &&
-  //       board[0][2] !== null)
-  //   )
-  //     return true;
-  //   return false;
-  // }
-
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((activePlayerSymbol, colIndex) => (
+            {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
-                  {activePlayerSymbol}
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol!==null}>
+                  {playerSymbol}
                 </button>
               </li>
             ))}
