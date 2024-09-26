8bf18ab4eaeef6107069653838e9049ef46a7e55
Kayhan <Kayhan@ulu.de>
2024-09-26 14:08:58 +0200

    Replace placeholder text in React components with actual HTML structure

--- code/05 React Practice Project/01-starting-project/src/components/Result.jsx
+++ code/05 React Practice Project/01-starting-project/src/components/Result.jsx
@@ -1,3 +1,18 @@
 export default function Result() {
-  return <section id="result">Result</section>;
+  return (
+    <section>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </section>
+  );
 }
--- code/05 React Practice Project/01-starting-project/src/components/UserInput.jsx
+++ code/05 React Practice Project/01-starting-project/src/components/UserInput.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 export default function UserInput() {
   return (
     <section id="user-input">
       <div className="input-group">
-        <p>Input 1</p>
+        <div>
+          <label htmlFor="initInvest">Initial Investment</label>
+          <input className="user-input" id="initInvest" type="number" />
+        </div>
+        <div>
+          <label htmlFor="anualInvest">Anual Investment</label>
+          <input className="user-input" id="anualInvest" type="number" />
+        </div>
+      </div>
+
+      <div className="input-group">
+        <div>
+          <label htmlFor="expectedReturn">Expected Return</label>
+          <input className="user-input" id="expectedReturn" type="number" />
+        </div>
+        <div>
+          <label htmlFor="duration">Duration</label>
+          <input className="user-input" id="duration" type="number" />
+        </div>
       </div>
     </section>
   );
