1ca9494d5b282126ecff40cdca0c550c509e46cd
Kayhan <Kayhan@ulu.de>
2024-09-30 12:33:05 +0200

    demo dynamic and conditional styling with Styled Componenents

--- code/06 Styling React Components/01-starting-project/src/components/Header.jsx
+++ code/06 Styling React Components/01-starting-project/src/components/Header.jsx
@@ -7,7 +7,6 @@ export default function Header() {
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p className={classes.paragraph}>A community of artists and art-lovers.</p>
-      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
