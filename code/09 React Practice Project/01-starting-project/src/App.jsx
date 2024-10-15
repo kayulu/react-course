ef30b1e8339556ed4b7141c91ee43ce13f3a5186
Kayhan <Kayhan@ulu.de>
2024-10-15 12:59:09 +0200

    add Button and NoProjectSelected cmps, useState for conditional rendering

--- code/09 React Practice Project/01-starting-project/src/App.jsx
+++ code/09 React Practice Project/01-starting-project/src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleCreateNewProject() {
+    setProjectsState((prevProjectState) => {
+      return {
+        ...prevProjectState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onCreateNewProject={handleCreateNewProject} />;
+  } else if(projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onCreateNewProject={handleCreateNewProject} />
+      {content}
     </main>
   );
 }
