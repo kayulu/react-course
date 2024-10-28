b73079e4a3829ddafd1df728d791214ab44ddf1c
Kayhan <Kayhan@ulu.de>
2024-10-28 21:38:32 +0100

    demo side-effect where there is no need to use useEffect() hook

--- code/11 Handling Side Effects/01-starting-project/src/App.jsx
+++ code/11 Handling Side Effects/01-starting-project/src/App.jsx
@@ -1,29 +1,39 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import { sortPlacesByDistance } from './loc.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import { sortPlacesByDistance } from "./loc.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+
+const storedIds = JSON.parse(localStorage.getItem("pickedPlaces")) || [];
+const storedPlaces =
+  storedIds.length > 0
+    ? storedIds.map((id) => AVAILABLE_PLACES.find((place) => place.id === id))
+    : [];
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   // this will be called after this component has returned
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
       setAvailablePlaces(sortedPlaces);
     });
-  },[])
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
-    selectedPlace.current = id;
+    selectedPlace.current = id; // useRef() to store a value without re-render
   }
 
   function handleStopRemovePlace() {
@@ -38,12 +48,22 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("pickedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem("pickedPlaces", JSON.stringify([id, ...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
+
+    const storedIds = JSON.parse(localStorage.getItem("pickedPlaces"));
+    const updatedIds = storedIds.filter((id) => id !== selectedPlace.current);
+    localStorage.setItem("pickedPlaces", JSON.stringify(updatedIds));
+
     modal.current.close();
   }
 
@@ -67,7 +87,7 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
